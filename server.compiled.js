4e8d02b11ccf5f70de1870f58d608573add1c156
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-22 03:07:57 -0500

    server

--- server.compiled.js
+++ server.compiled.js
@@ -12,16 +12,15 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 //
 var mysql = require('mysql');
 
-var app = (0, _express["default"])();
-app.use(cors()); // parse requests of content-type - application/json
+var app = (0, _express["default"])(); // parse requests of content-type - application/json
 
 app.use(_express["default"].json()); // parse requests of content-type - application/x-www-form-urlencoded
 
 app.use(_express["default"].urlencoded({
   extended: true
 }));
-app.use(_express["default"]["static"](_path["default"].join(__dirname, 'client', 'build')));
-app.use("/", _auth["default"]); // Handles any requests that don't match the ones above
+app.use("/", _auth["default"]);
+app.use(_express["default"]["static"](_path["default"].join(__dirname, 'client', 'build'))); // Handles any requests that don't match the ones above
 
 app.get('*', function (req, res) {
   res.sendFile(_path["default"].join(__dirname + '/client/build/index.html'));
--- server.js
+++ server.js
@@ -6,16 +6,13 @@ import router from './routes/auth.routes';
 const mysql = require('mysql');
 const app = express();
 
-app.use(cors());
-
 // parse requests of content-type - application/json
 app.use(express.json());
-
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use("/", router);
 
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
