f938ce19c28fd14e9d775015a34a5580fe53d3a8
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-22 02:41:48 -0500

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,7 @@ const controller = require("../controllers/auth.controller");
 const router = require("express").Router()
 
 router.post("/api/auth/signin", controller.signin)
-router.post("/api/auth/signup", controller.signup)
+router.post("http://zoointegrateddb-env.eba-j24arzz8.us-east-2.elasticbeanstalk.com:8080/api/auth/signup", controller.signup)
 router.post("/api/auth/employeesignin", controller.employeesignin)
 router.post("/api/auth/addanimal", controller.addanimal)
 router.post("/api/auth/addemployee", controller.addemployee)
