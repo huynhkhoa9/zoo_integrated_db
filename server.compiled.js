4e2449fef3078bb603ddebec9059eabe20d8908e
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-22 01:25:16 -0500

    server

--- server.compiled.js
+++ server.compiled.js
@@ -4,12 +4,10 @@ var _path = _interopRequireDefault(require("path"));
 
 var _express = _interopRequireDefault(require("express"));
 
-var _mysql$createConnecti;
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
+// server.js
+//
 var mysql = require('mysql');
 
 var app = (0, _express["default"])();
@@ -27,12 +25,22 @@ app.get('/flower', function (req, res) {
     colour: 'Blue-ish'
   });
 });
-var connection = mysql.createConnection((_mysql$createConnecti = {
-  host: process.env.RDS_HOSTNAME
-}, _defineProperty(_mysql$createConnecti, "host", process.env.RDS_HOSTNAME), _defineProperty(_mysql$createConnecti, "user", process.env.RDS_USERNAME), _defineProperty(_mysql$createConnecti, "password", process.env.RDS_PASSWORD), _defineProperty(_mysql$createConnecti, "database", process.env.RDS_HOSTNAME), _mysql$createConnecti));
-connection.connect();
+var connection = mysql.createConnection({
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT
+});
+connection.connect(function (err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+connection.end();
 var PORT = process.env.PORT || 8080;
 app.listen(PORT, function () {
   console.log("Server listening at port ".concat(PORT, "."));
 });
-connection.end();
--- server.js
+++ server.js
@@ -24,19 +24,25 @@ app.get('/flower', (req, res) => {
   });
 });
 
-const connection = mysql.createConnection({
-  host:      process.env.RDS_HOSTNAME,
-  host:      process.env.RDS_HOSTNAME,
-  user:      process.env.RDS_USERNAME,
-  password:  process.env.RDS_PASSWORD,
-  database:  process.env.RDS_HOSTNAME
+var connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
 });
 
-connection.connect();
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
+connection.end();
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}.`);
 });
-
-connection.end();
