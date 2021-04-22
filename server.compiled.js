89109c329a303e9b8b21e5ed325f9c2529e3e746
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-22 00:42:21 -0500

    Update server.compiled.js

--- server.compiled.js
+++ server.compiled.js
@@ -4,18 +4,17 @@ var _path = _interopRequireDefault(require("path"));
 
 var _express = _interopRequireDefault(require("express"));
 
+var _mysql$createPool;
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-// server.js
-//
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 var mysql = require('mysql');
 
-var db = mysql.createPool({
-  host: process.env.RDS_HOSTNAME,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  database: process.env.RDS_HOSTNAME
-});
+var db = mysql.createPool((_mysql$createPool = {
+  host: process.env.RDS_HOSTNAME
+}, _defineProperty(_mysql$createPool, "host", process.env.RDS_HOSTNAME), _defineProperty(_mysql$createPool, "user", process.env.RDS_USERNAME), _defineProperty(_mysql$createPool, "password", process.env.RDS_PASSWORD), _defineProperty(_mysql$createPool, "database", process.env.RDS_HOSTNAME), _mysql$createPool));
 var PORT = process.env.PORT || 8080;
 var app = (0, _express["default"])();
 app.use(_express["default"]["static"](_path["default"].join(__dirname, 'client', 'build')));
