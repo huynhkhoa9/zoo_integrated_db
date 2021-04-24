84521533c67c7719df9974586588de36c5a389a6
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-24 18:19:27 -0500

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -211,7 +211,7 @@ exports.animalReport =  function(req, res){
 
     pool.getConnection(function(err, connection){
         connection.query(listofAnimals, function(err, result, fields){
-            res.json(result);
+            res.send(result);
         });
         connection.release();
     });
