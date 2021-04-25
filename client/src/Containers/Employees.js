3b05c56d392d89d6dfc4c10c7b5b4f3390c42d97
EAJ2 <78275071+EAJ2@users.noreply.github.com>
2021-04-25 15:07:38 -0500

    update

--- client/src/Containers/Employees.js
+++ client/src/Containers/Employees.js
@@ -30,15 +30,19 @@ export default function Employee(){
             <EmployeeNavBar />
             <h1>Enter Employee Information</h1>
             <form class="employee" id="employeesinfo" onSubmit={handleSubmit7}>
+                <div>
                 <label>First Name</label>
                     <input type="text" placeholder="John" id="employeeFirstName" onChange={(e)=> setEmployeeFirstName(e.target.value)}></input>
-                
+                    </div>
+                    <div>
                 <label>Last Name</label>
                     <input type="text" placeholder="Snow" id="employeeLastName" onChange={(e)=> setEmployeeLastName(e.target.value)}></input>
-
+                    </div>
+                    <div>
                 <label>Date of Birth</label>
                     <input type="text" placeholder="12/31/2000" id="employeeDOB" onChange={(e)=> setEmployeeDOB(e.target.value)}></input>
-
+                    </div>
+                    <div>
                 <label>Gender</label>
                     <select id="employeeGender" onChange={(e)=> setEmployeeGender(e.target.value)}>
                         <option value="">Select Gender</option>  
@@ -46,10 +50,12 @@ export default function Employee(){
                         <option value="Female">Female</option>
                         <option value="Other">Other</option>
                     </select>
-
+                    </div>
+                    <div>
                 <label>Salary</label>
                     <input type="text" placeholder="15.00" id="employeeSalary"  onChange={(e)=> setEmployeeSalary(e.target.value)}></input>
-                
+                    </div>
+                    <div>
                 <label>Department</label>
                     <select id="department" onChange={(e)=> setEmployeeDepartment(e.target.value)}>
                         <option value="">Select Department</option>
@@ -67,22 +73,27 @@ export default function Employee(){
                         <option value="Membership Director">Membership Director</option>
                         <option value="Volunteer">Volunteer</option>
                     </select>
-
+                    </div>
+                    <div>
                 <label>Contact Information</label>
                     <input type="text" placeholder="713-000-0000" id="employeeContactInfo" onChange={(e)=> setEmployeeContactInfo(e.target.value)}></input>
-
+                    </div>
+                    <div>
                 <label>Social Security Number</label>
                     <input type="text" placeholder="000-00-0000" id="employeeSSN" onChange={(e)=> setEmployeeSSN(e.target.value)}></input>
-                
+                    </div>
+                    <div>
                 <label>Supervisor ID</label>
                     <input type="text" placeholder="12345" id="employeeSupervisorId" onChange={(e)=> setEmployeeSupervisorId(e.target.value)}></input>
-
+                    </div>
+                    <div>
                 <label>Employee ID</label>
                     <input type="text" placeholder="12345" id="employeeid" onChange={(e)=> setEmployeeId(e.target.value)}></input>
-
+                    </div>
+                    <div>
                 <label>New Employee Password</label>
                     <input type="text" placeholder="Enter a Password" id="employeepassword" onChange={(e)=> setEmployeePassword(e.target.value)}></input>
-
+                    </div>
                 <button type="submit">Submit</button>
             </form>
 
